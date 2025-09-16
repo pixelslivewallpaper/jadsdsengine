5814dd7181e2ef922f76821019fe087c91318cc1
Jadsds engine <pixelslivewallpaper@users.noreply.github.com>
2025-09-16 16:26:01 -0700

    Help files
    Help files

--- doce.js
+++ doce.js
@@ -1,9 +1,11 @@
 ﻿var jadsdsEngine = new JadsdsEngine('mycanvas');
 var jadsdsEngineB = new JadsdsEngine('mycanvasb');
+var jadsdsEngineC = new JadsdsEngine('mycanvasc');
 
 window.onload = function () {
     loadNewAnimationA("sf2newfbf");
     loadNewAnimationB("sf2dj");
+    loadNewAnimationC("toy");
 };
 
 function loadNewAnimationA(nameAni) {
@@ -18,6 +20,13 @@ function loadNewAnimationB(nameAni) {
     jadsdsEngineB.antiAlias = false;
     jadsdsEngineB.stopAnimationWhenIsNotVisible = true;
     jadsdsEngineB.loadAnimation(nameAni);
+}
+
+function loadNewAnimationC(nameAni) {
+    jadsdsEngineC.fixCanvasSize = false;
+    jadsdsEngineC.antiAlias = false;
+    jadsdsEngineC.stopAnimationWhenIsNotVisible = true;
+    jadsdsEngineC.loadAnimation(nameAni);
 }
 
 
